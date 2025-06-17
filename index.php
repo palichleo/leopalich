730dbdacb8b99fadd61dfce6ad5b18237f89ab36
palichleo <144258089+palichleo@users.noreply.github.com>
2025-06-17 14:08:32 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <title>Portfolio de Léopalich</title>
+</head>
+<body>
+  <h1>Bienvenue sur mon portfolio !</h1>
+  <p>Tout fonctionne, tu peux commencer à personnaliser.</p>
+</body>
+</html>
